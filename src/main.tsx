8326add013ccbe8d2a3541c3a6cd505ab8c99841
vibesgamesgamesrbx-png <vibesgamesgamesrbx@gmail.com>
2026-09-25 17:36:12 -0300

    Remove StrictMode double rendering for form stability

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { getRouter } from "./router";
@@ -6,7 +5,5 @@ import { getRouter } from "./router";
 const router = getRouter();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+  <RouterProvider router={router} />,
 );
